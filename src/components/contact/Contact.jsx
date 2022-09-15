324815826d6866ed799200c6b3baab24ce4efaa7
Bpeen Gurung <bipin67678grg@gmail.com>
2022-09-15 23:08:10 +0545

    add component(Contact with EmailJs)

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [emailSubmitDone, setEmailSubmitDon] = useState(false);
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_1ob41ej",
+        "template_ktk058t",
+        formRef.current,
+        "6mMIX1jrlhOFfuPmE"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setEmailSubmitDon(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="contact">
+      <div className="contact-bg"></div>
+      <div className="contact-wrapper">
+        <div className="contact-left">
+          <h1 className="contact-title">Lets discuss your projects</h1>
+          <div className="contact-info">
+            <div className="contact-info-item">
+              <img src={Phone} alt="Phone" className="contact-icon" />
+              +977-9819041679
+            </div>
+            <div className="contact-info-item">
+              <img src={Email} alt="addess" className="contact-icon" />
+              bipin67678grg@gmail.com
+            </div>
+            <div className="contact-info-item">
+              <img src={Address} alt="address" className="contact-icon" />
+              Kathmandu Nepal
+            </div>
+          </div>
+        </div>
+        <div className="contact-right">
+          <p className="contact-disc">
+            <b>What's your story.?</b>
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nisi,
+            nemo magnam vel maiores accusantium dolorum impedit illum sunt
+            debitis culpa natus placeat sed est dicta quae nesciunt distinctio
+            nihil.
+          </p>
+          <form onSubmit={submitHandle} ref={formRef}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subjects" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea
+              name="massege"
+              id=""
+              col="30"
+              rows="5"
+              placeholder="Massege"
+            ></textarea>
+            <button>Submit</button>
+            {emailSubmitDone && "Thank you..!"}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/data.js
+++ src/data.js
@@ -1,30 +1,31 @@
+import ProductImg from "./img/product-img.jpg"
 export const products = [
     {
         id: 1,
-        img: "https://github.com/Bipingrg-code/Easybank-landing-page/blob/main/design/desktop-design.jpg",
+        img: {ProductImg},
         link: "https://github.com/Bipingrg-code"
     },
     {
         id: 2,
-        img: "https://github.com/Bipingrg-code/Easybank-landing-page/blob/main/design/desktop-design.jpg",
+        img: {ProductImg},
         link: "https://github.com/Bipingrg-code"
     },
     {
         id: 3,
-        img: "https://github.com/Bipingrg-code/Easybank-landing-page/blob/main/design/desktop-design.jpg",
+        img: {ProductImg},
         link: "https://github.com/Bipingrg-code"
     },
     {
         id: 4,
-        img: "https://github.com/Bipingrg-code/Easybank-landing-page/blob/main/design/desktop-design.jpg",
+        img: {ProductImg},
         link: "https://github.com/Bipingrg-code"
     }, {
         id: 5,
-        img: "https://github.com/Bipingrg-code/Easybank-landing-page/blob/main/design/desktop-design.jpg",
+        img: {ProductImg},
         link: "https://github.com/Bipingrg-code"
     }, {
-        id: 6,
-        img: "https://github.com/Bipingrg-code/Easybank-landing-page/blob/main/design/desktop-design.jpg",
+        iid: 6,
+        img: {ProductImg},
         link: "https://github.com/Bipingrg-code"
     },
 ]
