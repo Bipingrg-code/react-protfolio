ebf6aa22f58397e496e5b2444940ee9cb478192b
Bpeen Gurung <bipin67678grg@gmail.com>
2022-09-17 22:48:47 +0545

    modify technology comp

--- src/components/technology/Tools.jsx
+++ src/components/technology/Tools.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./tools.css";
 import Html from "../../tools-icons/html.svg";
+import Css from "../../tools-icons/css.svg"
+import js from "../../tools-icons/js.svg"
+import { ThemeContext } from "../../context/Context";
 
 const Tools = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="tools-icons">
+    <div
+      className="tools-icons"
+      style={{ backgroundColor: darkMode && "#333" }}
+    >
       <h1 className="tools-icon-title">Technology I Used.</h1>
       <h3 className="tools-icons-disc">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, ea?
       </h3>
-     <div className="tools-img">
-     <div className="tools-icons-img">
-        <img src={Html} className="icons" alt="" srcset="" />
+      <div className="tools-img">
+        <div className="tools-icons-img">
+          <img src={Html} className="icons" alt="" srcset="" />
+        </div>
+        <div className="tools-icons-img">
+          <img src={Html} className="icons" alt="" srcset="" />
+        </div>
+        <div className="tools-icons-img">
+          <img src={Html} className="icons" alt="" srcset="" />
+        </div>
       </div>
-      <div className="tools-icons-img">
-        <img src={Html} className="icons" alt="" srcset="" />
-      </div>
-      <div className="tools-icons-img">
-        <img src={Html} className="icons" alt="" srcset="" />
-      </div>
-     </div>
     </div>
   );
 };
